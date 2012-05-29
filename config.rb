8c6c79dfe349abc9aabf21683598e9d5bb4b71be
Pak-Loon Cheng <pak.cheng@gmail.com>
2012-05-29 21:48:10 +0100

    Quick configuration

--- config.rb
+++ config.rb
@@ -2,7 +2,6 @@ require 'compass-h5bp'
 require 'html5-boilerplate'
 # Require any additional compass plugins here.
 
-
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
